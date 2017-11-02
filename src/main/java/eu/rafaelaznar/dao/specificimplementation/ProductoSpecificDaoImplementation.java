014f961f7eeb3905529c2a10efaa34db0d266dbc
Fernando <Fernando@192.168.1.70>
2017-11-02 17:47:54 +0100

    Primera version del programa

---
+++ src/main/java/eu/rafaelaznar/dao/specificimplementation/ProductoSpecificDaoImplementation.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.rafaelaznar.dao.specificimplementation;
+
+import eu.rafaelaznar.bean.specificimplementation.ProductoSpecificBeanImplementation;
+import eu.rafaelaznar.bean.specificimplementation.UsuarioSpecificBeanImplementation;
+import eu.rafaelaznar.dao.genericimplementation.TableGenericDaoImplementation;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Fernando
+ */
+public class ProductoSpecificDaoImplementation extends TableGenericDaoImplementation {
+    
+    public ProductoSpecificDaoImplementation(Connection oPooledConnection, UsuarioSpecificBeanImplementation oPuserBean_security, String strWhere) {
+        super("producto", oPooledConnection, oPuserBean_security, strWhere);
+    }
+
+   
+    
+}
